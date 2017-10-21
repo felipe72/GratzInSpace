ea0c7ff2893a0f96021162e292ae772164d09835
Harrison Pedro <hpedro1195@gmail.com>
2017-10-21 01:50:25 -0200

    Special sequence

---
+++ Assets/Scripts/KeySequenceController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySequenceController : MonoBehaviour
+{
+	public List<KeyCode> mList;
+    public List<KeyCode> mKeyList;
+    public int mCurrentIndex = 0;
+    public float allowedTimeBetweenmKeyList = 0.2f; //tweak as needed
+    private float timeLastButtonPressed;
+    public KeySequenceController(List<KeyCode> buttons)
+    {
+        mKeyList = buttons;
+        Debug.Log(mKeyList);
+		mList = new List<KeyCode>();
+    }
+
+    public bool Check()
+    {
+		Debug.Log("TIME " + timeLastButtonPressed);
+		Debug.Log("COUNT " + mList.Count);
+        if (Time.time > timeLastButtonPressed + allowedTimeBetweenmKeyList)
+        {
+            if (mCurrentIndex < mKeyList.Count && mList.Count > mCurrentIndex)
+            {
+                if (mKeyList[mCurrentIndex] == mList[mCurrentIndex])
+                {
+                        timeLastButtonPressed = Time.time;
+                        mCurrentIndex++;
+                }
+                if (mCurrentIndex >= mKeyList.Count)
+                {
+                    mCurrentIndex = 0;
+					//timeLastButtonPressed = 0;
+                    return true;
+                }
+                else{
+					if(mList.Count >= 3)
+						mList.Clear();
+                    return false;
+				}
+            }
+        }
+        return false;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	private Rigidbody2D rigidbody;
-	public float speed = 8f;
-	public GameObject shoot;
-	
-	void Start () {
-		rigidbody = this.GetComponent<Rigidbody2D>();
-	}
-	
-	void Update () {
-		float x = Input.GetAxis("Horizontal");
-		float y = Input.GetAxis("Vertical");
-		rigidbody.velocity = new Vector2(speed * x, speed * y);
-		if(Input.GetKeyDown(KeyCode.F)){
-			Instantiate(shoot, this.transform.position, this.transform.rotation);
+    private Rigidbody2D rigidbody;
+    public float speed = 8f;
+    public GameObject shoot;
+    private KeySequenceController combo1;
+	private List<KeyCode> keys;
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        combo1 = new KeySequenceController(new List<KeyCode> { KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.RightArrow });
+		keys = new List<KeyCode>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(speed * x, speed * y);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(shoot, this.transform.position, this.transform.rotation);
+        }
+
+		if(Input.GetKey(KeyCode.Z)){
+			if(Input.GetKeyDown(KeyCode.DownArrow))
+				combo1.mList.Add(KeyCode.DownArrow);
+			if(Input.GetKeyDown(KeyCode.RightArrow))
+				combo1.mList.Add(KeyCode.RightArrow);
 		}
-	}
+		if(combo1.mList.Count > 0){
+			if (combo1.Check()){
+            	Debug.Log("COMBOU!");
+				combo1.mList.Clear();
+        	}
+		}
+
+    }
 }
