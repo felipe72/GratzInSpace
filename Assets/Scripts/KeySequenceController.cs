723cbb80c46a2332c95e48fb3183293354b22d9b
Harrison Pedro <hpedro1195@gmail.com>
2017-10-21 04:31:36 -0200

    oi

--- Assets/Scripts/KeySequenceController.cs
+++ Assets/Scripts/KeySequenceController.cs
@@ -9,17 +9,15 @@ public class KeySequenceController : MonoBehaviour
     public int mCurrentIndex = 0;
     public float allowedTimeBetweenmKeyList = 0.2f; //tweak as needed
     private float timeLastButtonPressed;
-    public KeySequenceController(List<KeyCode> buttons)
+    public KeySequenceController()
     {
-        mKeyList = buttons;
-        Debug.Log(mKeyList);
+        mKeyList = this.RandomSequence();
 		mList = new List<KeyCode>();
     }
 
     public bool Check()
     {
-		Debug.Log("TIME " + timeLastButtonPressed);
-		Debug.Log("COUNT " + mList.Count);
+        Debug.Log(mKeyList[0] + " " + mKeyList[1] + " " + mKeyList[2]);
         if (Time.time > timeLastButtonPressed + allowedTimeBetweenmKeyList)
         {
             if (mCurrentIndex < mKeyList.Count && mList.Count > mCurrentIndex)
@@ -28,11 +26,15 @@ public class KeySequenceController : MonoBehaviour
                 {
                         timeLastButtonPressed = Time.time;
                         mCurrentIndex++;
+                }else{
+                    mList.Clear();
+                    return false;
                 }
                 if (mCurrentIndex >= mKeyList.Count)
                 {
                     mCurrentIndex = 0;
 					//timeLastButtonPressed = 0;
+                    mKeyList = this.RandomSequence();
                     return true;
                 }
                 else{
@@ -43,6 +45,18 @@ public class KeySequenceController : MonoBehaviour
             }
         }
         return false;
+    }
+
+    private List<KeyCode> RandomSequence(){
+        Debug.Log("ENTROU AQUI");
+        List<List<KeyCode>> randomSequence = new List<List<KeyCode>>();
+        randomSequence.Add(new List<KeyCode>(){KeyCode.RightArrow, KeyCode.RightArrow, KeyCode.RightArrow});
+        randomSequence.Add(new List<KeyCode>(){KeyCode.UpArrow, KeyCode.RightArrow, KeyCode.DownArrow});
+        randomSequence.Add(new List<KeyCode>(){KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.LeftArrow});
+        randomSequence.Add(new List<KeyCode>(){KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.DownArrow});
+        randomSequence.Add(new List<KeyCode>(){KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.UpArrow});
+        List<KeyCode> sequenceChosen = randomSequence[Random.Range(0, randomSequence.Count)];
+        return sequenceChosen;
     }
     // Use this for initialization
     void Start()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
@@ -10,10 +10,14 @@ public class PlayerController : MonoBehaviour
     public GameObject shoot;
     private KeySequenceController combo1;
 	private List<KeyCode> keys;
+    public Slider specialBarSlider;
+    public Text sequenceKeys;
+
+    public 
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody2D>();
-        combo1 = new KeySequenceController(new List<KeyCode> { KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.RightArrow });
+        combo1 = new KeySequenceController();
 		keys = new List<KeyCode>();
     }
 
@@ -22,22 +26,35 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         rigidbody.velocity = new Vector2(speed * x, speed * y);
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.Joystick1Button0) )
         {
+            if(specialBarSlider.value < 100)
+                specialBarSlider.value += 10;
             Instantiate(shoot, this.transform.position, this.transform.rotation);
         }
 
-		if(Input.GetKey(KeyCode.Z)){
+		if(Input.GetKey(KeyCode.Z) && specialBarSlider.value == 100){
+            sequenceKeys.text = combo1.mKeyList[0] + " " + combo1.mKeyList[1] + " " + combo1.mKeyList[2];
 			if(Input.GetKeyDown(KeyCode.DownArrow))
 				combo1.mList.Add(KeyCode.DownArrow);
+            
 			if(Input.GetKeyDown(KeyCode.RightArrow))
 				combo1.mList.Add(KeyCode.RightArrow);
+                
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+				combo1.mList.Add(KeyCode.UpArrow);
+            
+			if(Input.GetKeyDown(KeyCode.LeftArrow))
+				combo1.mList.Add(KeyCode.LeftArrow);
 		}
 		if(combo1.mList.Count > 0){
 			if (combo1.Check()){
             	Debug.Log("COMBOU!");
 				combo1.mList.Clear();
-        	}
+                specialBarSlider.value = 0;
+        	}else if(combo1.mList.Count == 3 && !combo1.Check()){
+                specialBarSlider.value = 0;
+            }
 		}
 
     }
