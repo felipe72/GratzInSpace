59a882d8b792cba2d98cc65bae0ec49cac5e3154
Felipe Osório <felipe.osorio72@outlook.com.br>
2017-10-20 23:08:40 -0200

    UI bom é UI implementado

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MenuController : MonoBehaviour {
+	public Image[] curtains;
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.K)) {
+			
+		}
+
+		if (Input.GetKeyDown (KeyCode.F)) {
+
+		}
+	}
+}
