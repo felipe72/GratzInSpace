58401be3f9179d77b48fd6a2799e7ca642aa7f6a
Simião <simiaosimis@gmail.com>
2017-10-21 02:22:05 -0300

    ExplodeShot

--- Assets/Scripts/Shoot/ExplodeShoot.cs
+++ Assets/Scripts/Shoot/ExplodeShoot.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using DG.Tweening;
 
 public class ExplodeShoot : MonoBehaviour {
+	PlayerController player;
+
+	void Start(){
+		player = FindObjectOfType<PlayerController> ();
+	}
+
 	void Update () {
 		Vector2 nextPosition = new Vector2(this.transform.position.x + 0.1f, this.transform.position.y);
 		this.transform.position = nextPosition;
@@ -13,7 +19,13 @@ public class ExplodeShoot : MonoBehaviour {
 	}
 	public void Explode(){
 		this.transform.DOScale(5f, 0.2f).SetEase(Ease.InQuad);
-		FindObjectOfType<PlayerController> ().exist = false;
+		player.exist = false;
 		Destroy(this.gameObject, 0.2f);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.gameObject.tag == "Enemy" && !player.exist){
+			Destroy(collider.gameObject);
+		}
 	}
 }
