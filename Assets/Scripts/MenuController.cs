56206dee036e0369e9bf3b7ef8a41371070bbd3f
Felipe Osório <felipe.osorio72@outlook.com.br>
2017-10-20 23:31:01 -0200

    Top

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,11 +9,11 @@ public class MenuController : MonoBehaviour {
 
 	void Update(){
 		if (Input.GetKeyDown (KeyCode.K)) {
-			
+			curtains [0].rectTransform.DOMoveY (curtains [0].rectTransform.position.y + 2000, 1);
 		}
 
 		if (Input.GetKeyDown (KeyCode.F)) {
-
+			curtains [1].rectTransform.DOMoveY (curtains [1].rectTransform.position.y - 2000, 1);
 		}
 	}
 }
